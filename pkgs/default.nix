e30807e40e7b9f1a91c928369a4e2f5f57d9ad8c
Bryan Prather-Huff <bryan@pratherhuff.com>
2026-07-19 05:44:41 -0500

    feat(pkgs): add gcloud-mcp

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -31,6 +31,7 @@ in
   deepl-mcp-server = pkgs.callPackage ./official/deepl { };
   esa-mcp-server = pkgs.callPackage ./official/esa { };
   freee-mcp = pkgs.callPackage ./official/freee { };
+  gcloud-mcp = pkgs.callPackage ./official/gcloud { };
   mastra-mcp-docs-server = pkgs.callPackage ./official/mastra { };
   nd-mcp = pkgs.callPackage ./official/netdata { };
   tavily-mcp = pkgs.callPackage ./official/tavily { };
