b0a55f982b25ad95c4ddf7c5a97c710288d9d3d1
Bryan Prather-Huff <bryan@pratherhuff.com>
2026-09-24 01:49:33 -0500

    fix: update flux-operator-mcp to 0.60.0

--- pkgs/official/flux-operator-mcp/default.nix
+++ pkgs/official/flux-operator-mcp/default.nix
@@ -8,27 +8,27 @@
 let
   assets = {
     aarch64-linux = {
-      name = "flux-operator-mcp_0.55.0_linux_arm64.tar.gz";
-      hash = "sha256-npG044UvHG6qeqanUSB+gTQ+Nb9WAMQ/pL9dmxxgGRc=";
+      name = "flux-operator-mcp_0.60.0_linux_arm64.tar.gz";
+      hash = "sha256-SOsnf6oZswODTgfDwRKJkzAseAebcSJzUkAyLGh8b/8=";
     };
     x86_64-linux = {
-      name = "flux-operator-mcp_0.55.0_linux_amd64.tar.gz";
-      hash = "sha256-UYYrBJNu9gabYxkdeOSZMsrIpyrv1auRUy8SsTRBKEw=";
+      name = "flux-operator-mcp_0.60.0_linux_amd64.tar.gz";
+      hash = "sha256-DpuqMmPLDXoTvKzul5ZIpmmoKIQO9bQVv4HHfXHX820=";
     };
     aarch64-darwin = {
-      name = "flux-operator-mcp_0.55.0_darwin_arm64.tar.gz";
-      hash = "sha256-adpOlaDRywej7wtFN3hvmKQIlaUnp1jCOa8z6tMBNzY=";
+      name = "flux-operator-mcp_0.60.0_darwin_arm64.tar.gz";
+      hash = "sha256-uc/Rhx/dkLbi9B8QoVRgLDofzL1VOjuMeOa7R2bL2D4=";
     };
     x86_64-darwin = {
-      name = "flux-operator-mcp_0.55.0_darwin_amd64.tar.gz";
-      hash = "sha256-F+7YCQkr/X0VE0eNs5s2DYHBd9AbXEZZK3Fv7VJ3f9M=";
+      name = "flux-operator-mcp_0.60.0_darwin_amd64.tar.gz";
+      hash = "sha256-BNH+TEzzCffX4D2taIKSD1YRZZg0qSctj/BhWWfy7a8=";
     };
   };
   asset = assets.${stdenvNoCC.hostPlatform.system};
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "flux-operator-mcp";
-  version = "0.55.0";
+  version = "0.60.0";
 
   src = fetchurl {
     url = "https://github.com/controlplaneio-fluxcd/flux-operator/releases/download/v${finalAttrs.version}/${asset.name}";
